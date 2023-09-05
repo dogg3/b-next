ca83a0e7c80fdb196b31fa65b064c73210643e66
Douglas Landvik <douglas.landvik@gmail.com>
2023-09-05 17:29:24 +0200

    feat(characterEvents): fixing comments

--- components/Form.js
+++ components/Form.js
@@ -1,36 +1,11 @@
 import React, {useEffect, useState} from 'react';
 
 
-export const ServiceType = {
-	WinterStay: {
-		label: "Placeringar", priceType: "SQM", variants: {
-			outdoor: {price: 500, label: "Utomhus", priceType: "SQM"},
-			inside: {price: 1100, label: "Inomhus", priceType: "SQM"},
-		},
-	}, EngineConservation: {
-		label: "Konservering av motorer, exklusive glykol", price: 1700, priceType: "unit",
-	}, BatteryConservation: {
-		label: "Konservering av elverk", price: 1700, priceType: "unit",
-	}, Batteries: {
-		label: "Batterivård", price: 450, priceType: "unit",
-	}, FreshwaterSystemConservation: {
-		label: "Konservering färskvattensystem", price: 1300, priceType: "unit",
-	}, SepticTankConservation: {
-		label: "Konservering av septitank", price: 1300, priceType: "unit",
-	}, HaulOut: {
-		label: "Upptagning, sjösättning och avpallning", price: 180, priceType: "SQM",
-	}, HullCleaning: {
-		label: "Bottentvätt", price: 60, priceType: "SQM",
-	}, ShrinkWrap: {
-		label: "Täckning med krymplast", price: 350, priceType: "SQM", filter: "outdoor",
-	},
-};
-
-
-export const BoatForm = () => {
+export const BoatForm = ({sType}) => {
 	const [boatLength, setBoatLength] = useState(0);
 	const [boatWidth, setBoatWidth] = useState(0);
 	const [companyJobs, setCompanyJobs] = useState();
+	const [ServiceType, setServiceType] = useState(sType);
 
 	//PriceObjects 
 	const [priceObject, setPriceObject] = useState([])
